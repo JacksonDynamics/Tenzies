edb58038c3f2708cfd8e7cf3d5c639879c7fad0f
studmanj <bjackso3@hotmail.com>
2023-01-16 19:09:28 -0700

    nano id

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 function App(){
   const [dice, setDice] = useState(allNewDice())
@@ -7,12 +8,16 @@ function App(){
   function allNewDice() {
     const newDice = []
     for(let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push({
+        value:Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()  
+      })
     }
     return newDice
   }
 
-  const diceElement = dice.map(die => <Die value={die} />)
+  const diceElement = dice.map(die => <Die key={die.id} value={die.value} />)
 
   function handleClick(){
     setDice(allNewDice())
